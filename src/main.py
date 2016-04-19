bda7c9b3504392792c8a88f52d2a7368ec552726
Artem Dudarev <dudarev@gmail.com>
2016-04-19 13:27:16 +0300

    Main page

--- src/main.py
+++ src/main.py
@@ -1,14 +1,41 @@
-from flask import Flask
+from google.appengine.api import memcache
+from flask import Flask, render_template, request
+
+from models import Question
 
 app = Flask(__name__)
 
 
 import admin
+import cron
+import questions
+
+
+TOTAL_COUNT_CACHE_TIME = 3600
+QUESTIONS_PER_PAGE = 20
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def main():
+    start = int(request.args.get('start', 0))
+    questions = Question.query().order(-Question.created_at).fetch(QUESTIONS_PER_PAGE, offset=start)
+    total_questions_count = memcache.get('total_questions_count')
+    if not total_questions_count:
+        total_questions_count = Question.query().count()
+        memcache.set('total_questions_count', total_questions_count, TOTAL_COUNT_CACHE_TIME)
+    if total_questions_count > start + QUESTIONS_PER_PAGE:
+        next_start = start + QUESTIONS_PER_PAGE
+    else:
+        next_start = 0
+    previous_start = start - QUESTIONS_PER_PAGE
+    if previous_start < 0:
+        previous_start = 0
+    context = {
+        'next_start': next_start,
+        'previous_start': previous_start,
+        'questions': questions,
+    }
+    return render_template('index.html', **context)
 
 
 if __name__ == '__main__':
